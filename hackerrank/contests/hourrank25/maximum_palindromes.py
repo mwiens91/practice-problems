fdb93363e0558a934128aaf0557c3bea0d252c77
Matt Wiens <mwiens91@gmail.com>
2018-02-01 14:38:38 -0800

    Maximum Palindromes: add more to solution
    Logic should be okay for the most part. Still needs optimization. Still
    need to implement the answer mod 10^9 + 7.

--- hackerrank/contests/hourrank25/maximum_palindromes.py
+++ hackerrank/contests/hourrank25/maximum_palindromes.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
+import math
 import re
 import sys
 
 
-def find_num_palindromes(i, j):
-    """Counts palindromes.
+def find_num_max_palindromes(i, j):
+    """Counts number of max length palindromes.
 
-    Finds the number of palindromes inside substring marked by i, j.
+    Finds the number of maximum length palindromes inside substring
+    marked by i, j.
     """
     # Explicitly using the outside counts list
     global counts
+
+    # Deal with the explicit trivial cases here
+    if j - i <= 1:
+        return 1
 
     # Count letters in the substring
     if not i:
@@ -18,21 +24,39 @@ def find_num_palindromes(i, j):
     else:
         count = [b - a for a, b in zip(counts[i - 1], counts[j])]
 
-    # Count number of even pairs of letters. Abort if there is more than
-    # one odd number of a letter
+    # Count number of even pairs of letters. And the number of duplicate
+    # pairs for each letter if there are any. Abort if there is more
+    # than one odd number of a letter.
     pairs = 0
     odds = 0
+    duplicate_list = []
     for lettercount in count:
-        if lettercount % 2:
+        # Check if there are any, an odd number, or an even number of a
+        # letter
+        if not lettercount:
+            continue
+        elif lettercount % 2:
             odds += 1
 
             # Abort if more than one odd number of a letter
             if odds == 2:
                 return 0
         else:
-            pairs += lettercount / 2
+            numpairs = lettercount / 2
 
-    return pairs
+            # Add duplicate pairs if needed
+            if numpairs > 1:
+                duplicate_list.append(numpairs)
+
+            # Add the number of pairs to the total sum
+            pairs += numpairs
+
+    # This is a (possibly well known?) discrete math formula
+    num_palindromes = math.factorial(pairs)
+    for duplicate in duplicate_list:
+        num_palindromes /= math.factorial(duplicate)
+
+    return num_palindromes
 
 
 # Read lines from stdin
@@ -60,4 +84,18 @@ for idx, letter in enumerate(s):
 
 # Now go through substrings
 for l, r in testindices:
-    find_num_palindromes(l,r)
+    # Shrink the search range each time we don't find a palindrome
+    for delta in range(r - l + 1):
+        best = 0
+
+        # Distribute the delta shrinking over left and right indices
+        for leftdelta in range(delta + 1):
+            best += (
+                find_num_max_palindromes(l + leftdelta, r - (delta-leftdelta)))
+
+        # If there are any maximum length permutations for this delta,
+        # print it and move on to the next substring, otherwise shrink
+        # delta further
+        if best:
+            print(best)
+            break
