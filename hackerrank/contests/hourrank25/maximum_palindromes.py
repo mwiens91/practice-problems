fec237b2ef4b53adf5838ec34a1a087dce432803
Matt Wiens <mwiens91@gmail.com>
2018-02-01 17:03:27 -0800

    Maximum Palindromes: fix misunderstanding

--- hackerrank/contests/hourrank25/maximum_palindromes.py
+++ hackerrank/contests/hourrank25/maximum_palindromes.py
@@ -25,8 +25,7 @@ def find_num_max_palindromes(i, j):
         count = [b - a for a, b in zip(counts[i - 1], counts[j])]
 
     # Count number of even pairs of letters. And the number of duplicate
-    # pairs for each letter if there are any. Abort if there is more
-    # than one odd number of a letter.
+    # pairs for each letter if there are any.
     pairs = 0
     odds = 0
     duplicate_list = []
@@ -37,10 +36,6 @@ def find_num_max_palindromes(i, j):
             continue
         elif lettercount % 2:
             odds += 1
-
-            # Abort if more than one odd number of a letter
-            if odds == 2:
-                return 0
         else:
             numpairs = int(lettercount / 2)
 
@@ -51,15 +46,23 @@ def find_num_max_palindromes(i, j):
             # Add the number of pairs to the total sum
             pairs += numpairs
 
+
     # This is using a (possibly well known?) discrete math formula
     # First calculate the factorial mod M of the pairs
     ans = 1
-    for i in range(1, pairs+1):
-        ans = ans * i % M
+    for k in range(1, pairs+1):
+        ans = ans * k % M
 
     # Now divide by the factorial of each duplicate using fancy math
     for duplicate in duplicate_list:
         ans = ans * pow(math.factorial(duplicate), M-2, M) % M
+
+    # Multiply by the number of odd numbers - 1 if there are any odd
+    # numbers
+    if not odds:
+        pass
+    else:
+        ans = ans * odds % M
 
     return ans
 
@@ -92,20 +95,4 @@ for idx, letter in enumerate(s):
 
 # Now go through substrings
 for l, r in testindices:
-    # Shrink the search range each time we don't find a palindrome
-    for delta in range(r - l + 1):
-        best = 0
-
-        # Distribute the delta shrinking over left and right indices
-        for leftdelta in range(delta + 1):
-            best += (
-                find_num_max_palindromes(l + leftdelta, r - (delta-leftdelta)))
-            best %= M
-
-        # If there are any maximum length permutations for this delta,
-        # print it and move on to the next substring, otherwise shrink
-        # delta further
-        if best:
-            # Print answer modulo M
-            print(best)
-            break
+    print(find_num_max_palindromes(l, r))
