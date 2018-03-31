eb8ebac2544c5bacefee2fadca79f757d1ec4453
Matt Wiens <mwiens91@gmail.com>
2018-03-30 18:44:40 -0700

    Shell: add an awk problem

---
+++ hackerrank/shell/grep-sed-awk/awk_03.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+awk '{
+
+avg = ($2 + $3 + $4) / 3
+
+if (avg >= 80)
+    grade = "A"
+else if (avg >= 60)
+    grade = "B"
+else if (avg >= 50)
+    grade = "C"
+else
+    grade = "FAIL"
+
+print $0, ":", grade
+
+}'
