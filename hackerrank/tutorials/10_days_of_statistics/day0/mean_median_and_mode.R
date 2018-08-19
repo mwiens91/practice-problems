a3d8f65f83d05ce39238f6ba59d447b8967ff47b
Matt Wiens <mwiens91@gmail.com>
2018-08-18 20:53:22 -0700

    10 Days of Statistics: follow Google R style tips

--- hackerrank/tutorials/10_days_of_statistics/day0/mean_median_and_mode.R
+++ hackerrank/tutorials/10_days_of_statistics/day0/mean_median_and_mode.R
@@ -1,10 +1,10 @@
 #!/usr/bin/env Rscript
 
 
-# Function to find statistical mode. Thanks to Ken Williams on
-# StackOverflow for his post at
-# https://stackoverflow.com/questions/2547402/is-there-a-built-in-function-for-finding-the-mode
 Mode <- function(x) {
+  # Function to find statistical mode. Thanks to Ken Williams on
+  # StackOverflow for his post at
+  # https://stackoverflow.com/questions/2547402/is-there-a-built-in-function-for-finding-the-mode
   x <- sort(x)
   ux <- unique(x)
   ux[which.max(tabulate(match(x, ux)))]
@@ -18,11 +18,11 @@ lines <- readLines(file("stdin"))
 numbers <- as.numeric(unlist(strsplit(lines[2], split=" ")))
 
 # Compute results
-result_mean <- mean(numbers)
-result_median <- median(numbers)
-result_mode <- Mode(numbers)
+result.mean <- mean(numbers)
+result.median <- median(numbers)
+result.mode <- Mode(numbers)
 
 # Print results
-cat(round(result_mean, 1), sep="\n")
-cat(round(result_median, 1), sep="\n")
-cat(result_mode, sep="\n")
+cat(round(result.mean, 1), sep="\n")
+cat(round(result.median, 1), sep="\n")
+cat(result.mode, sep="\n")
