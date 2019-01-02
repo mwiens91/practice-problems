fe60959cd61a859344b670bee631125ad18823d1
Matt Wiens <mwiens91@gmail.com>
2019-01-02 15:45:44 -0800

    Remove accidentally committed temp file

--- hackerrank/functional-programming/ad-hoc/asdf.hs
+++
@@ -1,7 +0,0 @@
--- Find a prime factor—not very efficient
-findPrimeFactor :: Integer -> Integer
-findPrimeFactor 1 = 1
-findPrimeFactor 2 = 2
-findPrimeFactor 3 = 3
-findPrimeFactor n = if null factors then 1 else head factors
-    where factors = filter (\x -> (n `mod` x) == 0) [2 .. n - 1]
