bf9811b72b83a21455b4f615e97cbee29dda0250
Matt Wiens <mwiens91@gmail.com>
2019-01-06 21:42:19 -0800

    Use 2 spaces per indent with Lua

--- hackerrank/algorithms/warmup/diagonal_difference.lua
+++ hackerrank/algorithms/warmup/diagonal_difference.lua
@@ -5,11 +5,11 @@ local n = io.read("*n", "*l")
 local arr = {}
 
 for i = 1, n do
-    arr[i] = {}
+  arr[i] = {}
 
-    for token in string.gmatch(io.read("*l"), "[^%s]+") do
-        table.insert(arr[i], tonumber(token:match("(.-)%s*$")))
-    end
+  for token in string.gmatch(io.read("*l"), "[^%s]+") do
+    table.insert(arr[i], tonumber(token:match("(.-)%s*$")))
+  end
 end
 
 -- Sum the main diagonal of the matrix and substract the sum of the
@@ -17,7 +17,7 @@ end
 local sum = 0
 
 for i = 1, n do
-    sum = sum + arr[i][i] - arr[i][n + 1 - i]
+  sum = sum + arr[i][i] - arr[i][n + 1 - i]
 end
 
 sum = math.abs(sum)
