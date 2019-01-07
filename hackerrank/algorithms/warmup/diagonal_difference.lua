70b8257d208d542be8ef01650bc7bb2aba7b19b3
Matt Wiens <mwiens91@gmail.com>
2019-01-06 20:10:32 -0800

    Algorithms: solve easy problem with Lua

---
+++ hackerrank/algorithms/warmup/diagonal_difference.lua
@@ -0,0 +1,26 @@
+#!/usr/bin/env lua
+
+-- Read input
+local n = io.read("*n", "*l")
+local arr = {}
+
+for i = 1, n do
+    arr[i] = {}
+
+    for token in string.gmatch(io.read("*l"), "[^%s]+") do
+        table.insert(arr[i], tonumber(token:match("(.-)%s*$")))
+    end
+end
+
+-- Sum the main diagonal of the matrix and substract the sum of the
+-- off-diagonal
+local sum = 0
+
+for i = 1, n do
+    sum = sum + arr[i][i] - arr[i][n + 1 - i]
+end
+
+sum = math.abs(sum)
+
+-- Print the result
+print(sum)
