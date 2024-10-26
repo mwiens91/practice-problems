5743471a978fe6db8473600d407ca6499758765f
Matt Wiens <mwiens91@gmail.com>
2024-10-25 19:19:18 -0700

    Use 3-pointer solution for 3sum

--- leetcode/15.3sum.cpp
+++ leetcode/15.3sum.cpp
@@ -1,76 +1,50 @@
 // @leet start
-#include <ranges>
-#include <set>
-#include <unordered_map>
+#include <algorithm>
 #include <vector>
 
 class Solution {
 public:
   std::vector<std::vector<int>> threeSum(std::vector<int> &nums) {
-    // Put our input numbers in a map. Frequency map has numbers as keys and
-    // their frequency as values.
-    std::unordered_map<int, int> freqMap;
-
-    for (const auto &x : nums) {
-      if (freqMap.contains(x)) {
-        ++freqMap[x];
-      } else {
-        freqMap[x] = 1;
-      }
-    }
-
-    // Put all the keys in a vector to iterate over
-    std::vector<int> uniqueNums;
-    uniqueNums.reserve(freqMap.size());
-
-    for (const auto &x : std::views::keys(freqMap)) {
-      uniqueNums.push_back(x);
-    }
-
-    // We'll use this to keep track of numbers we've seen so far as we
-    // iterate through. This will be used in the third chunk of the main
-    // loop below.
-    std::set<int> numsSeen;
-
-    // Find all possible solutions
+    // Hold the solutions
     std::vector<std::vector<int>> results;
 
-    for (const auto &[i, x] : std::views::enumerate(uniqueNums)) {
-      // Mark that we've seen x for future iterations
-      numsSeen.insert(x);
+    // Sort the input
+    std::sort(nums.begin(), nums.end());
 
-      // Test for solutions containing exactly three 'x's, which happens
-      // only when x == 0
-      if (x == 0 && freqMap[x] >= 3) {
-        results.push_back({x, x, x});
+    // The idea here is similar to two-sum ii (problem 167). Here we'll
+    // fix an i, and let its negative be the desired target, and then
+    // run something similar to two-pointer solution to two-sum ii.
+    for (size_t i = 0; i < nums.size() - 2; i++) {
+      // Make sure we don't do-over a duplicate value
+      if (i > 0 && nums[i] == nums[i - 1]) {
+        continue;
       }
 
-      // Test for solutions containing exactly two 'x's
-      if (freqMap[x] >= 2) {
-        // Get complement z
-        const auto z = -2 * x;
+      const auto x = nums[i];
 
-        if (freqMap.contains(z) && z != 0) {
-          results.push_back({x, x, z});
-        }
-      }
+      // The following is basically two-sum ii except we're gathering
+      // *all* solutions, not just one. See two-sum ii solution for
+      // comments on the following code.
+      size_t j = i + 1;
+      size_t k = nums.size() - 1;
 
-      // Test for solutions containing exactly one 'x' using numbers we
-      // haven't iterated over yet. We'll defer handling solutions
-      // {x, y, z} with y == z for other iterations of this loop using
-      // the block above for handling exactly two of a given number.
-      for (const auto &[j, y] :
-           std::views::enumerate(uniqueNums | std::views::drop(i + 1))) {
-        // Get complement z
-        const auto z = -x - y;
-
-        // Make sure we don't have two 'x's or 'y's
-        if (x == z || y == z) {
+      for (; j < k; ++j) {
+        if (j > i + 1 && nums[j] == nums[j - 1]) {
           continue;
         }
 
-        if (numsSeen.contains(z)) {
-          results.push_back({x, y, z});
+        const auto y = nums[j];
+        const auto complement = -x - y;
+
+        for (; j < k ; --k) {
+          const auto z = nums[k];
+
+          if (z == complement) {
+            results.push_back({x, y, z});
+            break;
+          } else if (z < complement) {
+            break;
+          }
         }
       }
     }
