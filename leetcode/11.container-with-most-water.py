6329816b45671e9c42bc1fbb6f4e2d5cba87463d
Matt Wiens <mwiens91@gmail.com>
2024-10-29 15:21:52 -0700

    Add container with most water

---
+++ leetcode/11.container-with-most-water.py
@@ -0,0 +1,23 @@
+# @leet start
+class Solution:
+    def maxArea(self, height: list[int]) -> int:
+        # We'll use a forwards and backwards iterator to compute
+        # candidates for the best area
+        i = 0
+        j = len(height) - 1
+
+        best = 0
+
+        while i < j:
+            best = max(best, (j - i) * min(height[i], height[j]))
+
+            # Move the iterator which points to a shorter height. The
+            # larger height can potentially hold more water so we want
+            # to keep it.
+            if height[i] > height[j]:
+                j -= 1
+            else:
+                i += 1
+
+        return best
+# @leet end
