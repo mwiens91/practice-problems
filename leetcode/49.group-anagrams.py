7ec41eebe052146ce2cfee36a7b27c393db22f4e
Matt Wiens <mwiens91@gmail.com>
2024-10-30 10:08:56 -0700

    Add improvement note to group anagrams

--- leetcode/49.group-anagrams.py
+++ leetcode/49.group-anagrams.py
@@ -1,14 +1,20 @@
 # @leet start
-from collections import Counter
-
 class Solution:
     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
-        # For each string, group them together if their sorted strings
+        # For each string, group them together if their character counts
         # are equal. group_dict has a sorted string as a key and a list
         # of strings that much that counter as a value.
         group_dict = {}
 
         for str_ in strs:
+            # TODO: If we let m indicate the maximum length of strings
+            # given and n indicate the number of strings we're given,
+            # then sorting here makes the time complexity O(nm logm).
+            # We might be able to get this down to O(nm) if we find some
+            # way to hash a counter of each string and use this as a
+            # key. Probably not hard. Does need a bit of
+            # thinking/Googling. Not going to bother doing this here.
+            # Rather move on.
             if (sorted_str := ''.join(sorted(str_))) in group_dict:
                 group_dict[sorted_str].append(str_)
             else:
