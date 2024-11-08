a866c38bf86dd2415fc264e29900726f0c4f5ff8
Matt Wiens <mwiens91@gmail.com>
2024-11-07 16:47:57 -0800

    Add two easy LeetCode problems

---
+++ leetcode/14.longest-common-prefix.py
@@ -0,0 +1,33 @@
+# @leet start
+import itertools
+
+class Solution:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        # Number of strings passed in
+        n_strings = len(strs)
+
+        # Find longest prefix string
+        longest_prefix = ""
+
+        str_idx = 0
+
+        # For this loop, we keep iterating until we find a character
+        # that is not common at a given index, or until we exhaust the
+        # shortest string
+        try:
+            while True:
+                common_char = strs[0][str_idx]
+
+                # Test if all strings have the same character at str_idx
+                for str_ in itertools.islice(strs, 1, n_strings):
+                    assert str_[str_idx] == common_char
+
+                # Update
+                str_idx += 1
+                longest_prefix += common_char
+
+        except (AssertionError, IndexError):
+            pass
+
+        return longest_prefix
+# @leet end
---
+++ leetcode/28.find-the-index-of-the-first-occurrence-in-a-string.py
@@ -0,0 +1,44 @@
+# @leet start
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        # Use two pointers, i will keep moving forward until we find the
+        # first character of needle; once and if that is found j will
+        # traverse needle and haystack simulataneously looking for a
+        # match. If a match is found, we return i; else we keep moving i
+        # forward.
+        #
+        # Note that we can't just set i to j after a potential match
+        # fails. See the following example:
+        #
+        # needle: loele
+        # haystack substring: loeloele
+        #                     ^   ^
+        #                     i   j
+        # Setting i to j after the initial match fails causes us to miss
+        # the actual first occurance.
+        len_haystack = len(haystack)
+        len_needle = len(needle)
+
+        # Edge case
+        if len_needle > len_haystack:
+            return -1
+
+        for i in range(len_haystack - len_needle + 1):
+            # Check for a potential match
+            try:
+                haystack_ch = haystack[i]
+
+                if haystack_ch == needle[0]:
+                    # See if the rest is a match
+                    for j in range(1, len(needle)):
+                        assert haystack[i + j] == needle[j]
+
+                    # We got a match!
+                    return i
+
+            except AssertionError:
+                pass
+
+        # Failed to find occurance of needle in haystack
+        return -1
+# @leet end
