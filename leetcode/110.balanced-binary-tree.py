448688e5ab1ba241cd87ad14f7e8d586f4454268
Matt Wiens <mwiens91@gmail.com>
2024-12-13 00:15:19 -0800

    Add another leetcode problem

---
+++ leetcode/110.balanced-binary-tree.py
@@ -0,0 +1,37 @@
+# @leet start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: ListNode | None) -> bool:
+        # Get the depth of the subtree rooted at a node. Also assert
+        # along the way that each node is balanced. For the case of
+        # comparing depths, we take null nodes as having a depth of -1.
+        def get_subtree_depth_rooted_at_node(node: ListNode | None) -> int:
+            # Base cases: null node
+            if node is None:
+                return -1
+
+            # Get depths of left and right subtrees
+            left_subtree_depth = get_subtree_depth_rooted_at_node(node.left)
+            right_subtree_depth = get_subtree_depth_rooted_at_node(node.right)
+
+            # Assert left and right subtrees balanced
+            assert abs(left_subtree_depth - right_subtree_depth) <= 1
+
+            # Return the depth of this subtree
+            return 1 + max(left_subtree_depth, right_subtree_depth)
+
+        # If there's no assertion error when running above function on
+        # root, it's balanced, otherwise it isn't.
+        try:
+            get_subtree_depth_rooted_at_node(root)
+        except AssertionError:
+            return False
+
+        return True
+
+# @leet end
