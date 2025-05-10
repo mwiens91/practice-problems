83807c0a7dbb6b054890571e7a4309cb133d00e0
Matt Wiens <mwiens91@gmail.com>
2025-05-09 17:53:00 -0700

    Add missing 561 solution
    Didn't write the buffer to the file for some reason.

--- leetcode/561.array-partition.py
+++ leetcode/561.array-partition.py
@@ -1,5 +1,8 @@
-# @leet start
 class Solution:
-    def arrayPairSum(self, nums: List[int]) -> int:
-        
-# @leet end
+    def arrayPairSum(self, nums: list[int]) -> int:
+        # Use greed. Sort the numbers; the optimized pairings are
+        # each even-indexed element with its next element. The maximized
+        # sum is thus the sum of all even-indexed elements.
+        nums.sort()
+
+        return sum(nums[::2])
