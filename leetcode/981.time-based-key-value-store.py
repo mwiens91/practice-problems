8daf5152bd8b1399b62a093ed05a100ee429fadf
Matt Wiens <mwiens91@gmail.com>
2026-05-03 15:35:43 -0700

    Redo more Grind 75 with Python

--- leetcode/981.time-based-key-value-store.py
+++ leetcode/981.time-based-key-value-store.py
@@ -5,52 +5,29 @@ class TimeMap:
         self.map: dict[str, list[tuple[int, str]]] = {}
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        try:
-            self.map[key].append((timestamp, value))
-        except KeyError:
-            self.map[key] = [(timestamp, value)]
+        if key not in self.map:
+            self.map[key] = []
+
+        self.map[key].append((timestamp, value))
 
     def get(self, key: str, timestamp: int) -> str:
-        # Return empty string if no values with key or timestamp is less
-        # than all keys
-        try:
-            value_tuples: list[tuple[int, str]] = self.map[key]
-
-            smallest_timestamp = value_tuples[0][0]
-
-            assert smallest_timestamp <= timestamp
-        except (KeyError, AssertionError):
+        if key not in self.map:
             return ""
 
-        # Do a binary search to find the largest timestamp_prev to
-        # return the value of such that timestamp_prev <= timestamp
-        left = 0
-        right = len(value_tuples) - 1
+        arr = self.map[key]
 
-        timestamp_prev_diff = abs(smallest_timestamp - timestamp)
-        timestamp_prev_idx = 0
+        left = 0
+        right = len(arr) - 1
 
         while left <= right:
             mid = (left + right) // 2
-            mid_timestamp = value_tuples[mid][0]
 
-            if mid_timestamp == timestamp:
-                timestamp_prev_idx = mid
-
-                break
-
-            if mid_timestamp < timestamp:
-                # Update best timestamp_prev candidate
-                if (diff := abs(mid_timestamp - timestamp)) < timestamp_prev_diff:
-                    timestamp_prev_idx = mid
-                    timestamp_prev_diff = diff
-
+            if arr[mid][0] <= timestamp:
                 left = mid + 1
             else:
-                # mid_timestamp > timestamp
                 right = mid - 1
 
-        return value_tuples[timestamp_prev_idx][1]
+        return arr[right][1] if right != -1 else ""
 
 
 # Your TimeMap object will be instantiated and called as such:
