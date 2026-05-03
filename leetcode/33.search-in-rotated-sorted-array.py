e90689742b104c4fd640783a3657bf1bf9a6f821
Matt Wiens <mwiens91@gmail.com>
2026-05-02 21:13:21 -0700

    Add another leetcode problem

--- leetcode/33.search-in-rotated-sorted-array.py
+++ leetcode/33.search-in-rotated-sorted-array.py
@@ -1,68 +1,47 @@
 # @leet start
-from bisect import bisect_left
-
-
-def find_pivot(i, j, nums):
-    """Return the pivot k.
-
-    i is the starting index of subarray we're looking at; j is the
-    ending index; both inclusive. Here we assume that i < j. We'll
-    handle i == j elsewhere. (i > j should never happen.)
-
-    The pivot k is given by k = n - j whenever we reach a base case. If
-    the array isn't rotated, we just return None.
-    """
-    # Base case
-    if j == i + 1:
-        if nums[j] < nums[i]:
-            # Found pivot k
-            return len(nums) - j
-
-        # No pivot k
-        return None
-
-    # Middle point of subarray nums[i:j] (j inclusive)
-    mid_idx = i + (j - i) // 2
-
-    # Recurse
-    if nums[i] > nums[mid_idx]:
-        return find_pivot(i, mid_idx, nums)
-
-    return find_pivot(mid_idx, j, nums)
-
-
 class Solution:
     def search(self, nums: list[int], target: int) -> int:
-        # Find pivot k
-        nums_length = len(nums)
+        n = len(nums)
 
-        # No rotation possible for length 1 array
-        if nums_length == 1:
-            k = None
-        else:
-            k = find_pivot(0, nums_length - 1, nums)
+        # Find smallest element, call its index "pivot"
+        left = 0
+        right = n - 1
 
-        # Now bisect to find if target is in nums. Which subarray we
-        # search in depends on the rotation.
-        lo = 0
-        hi = nums_length
+        while left <= right:
+            mid = (left + right) // 2
 
-        if k is None:
-            # Search whole array
-            pass
-        elif target <= nums[nums_length - 1]:
-            # Search the beginning part of the non-rotated array
-            lo = nums_length - k
-        else:
-            # Search the ending part of the non-rotated array
-            hi = nums_length - k
+            # Exit early optimization
+            if nums[mid] == target:
+                return mid
 
-        # target_idx is where target *should* be if it's in the array.
-        # If target is greater than everything in the array then
-        # target_idx will be whatever hi is set to.
-        target_idx = bisect_left(nums, target, lo, hi)
+            # Equality handles degenerate n = 1 case
+            if nums[mid - 1] >= nums[mid]:
+                pivot = mid
+                break
 
-        return target_idx if target_idx != hi and nums[target_idx] == target else -1
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        # Find target. Do normal binary search and factor in pivot
+        # when looking up mid value.
+        left = 0
+        right = n - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            true_mid = (mid + pivot) % n
+
+            if nums[true_mid] == target:
+                return true_mid
+
+            if nums[true_mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return -1
 
 
 # @leet end
